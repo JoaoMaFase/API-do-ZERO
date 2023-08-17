1f0eadbf136cad26b807fda77e7c6a976f1afa91
JoaoMaFase <nascc.joao@gmail.com>
2023-08-17 09:05:40 -0300

    ok

--- Api/src/controller/usuarioController.js
+++ Api/src/controller/usuarioController.js
@@ -7,11 +7,14 @@ server.post('/usuario/login', async (req,resposta) => {
     try {
         const { email, senha } = req.body;
         const response = await login(email,senha)
+        if (!response){
+            throw new Error('Credenciais inválidas')
+        }
         resposta.send(response)
 
     }   catch (err) {
-        resposta.status(400).send({
-            erro: "Ocorreu um erro lascado"
+        resposta.status(401).send({
+            erro: err.message
         })
     }
 })
